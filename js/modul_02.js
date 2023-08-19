0e43e220f978de7de3cbe3ba932a61ff3c92ad0d
AmmelyStar <113164996+AmmelyStar@users.noreply.github.com>
2023-08-19 19:41:19 +0200

    Create modul_02.js

---
+++ js/modul_02.js
@@ -0,0 +1,282 @@
+// 1.
+// Запиши умову в інструкції if таким чином, щоб функція працювала правильно.
+
+// Оголошена функція checkAge(age).
+// У виразі перевірки віку використаний оператор >=
+// Виклик checkAge(20) повертає "You are an adult"
+// Виклик checkAge(8) повертає "You are a minor"
+// Виклик checkAge(14) повертає "You are a minor"
+// Виклик checkAge(38) повертає "You are an adult"
+// В тілі функції є тільки одна інструкція if
+// В тілі функції відсутні інструкції else або else if
+
+// function checkAge(age) {
+//   if (age >= 18) { // Change this line
+//     return "You are an adult";
+//   }
+
+//   return "You are a minor";
+// }
+// ********************************************************************
+// 2.
+// Функція checkPassword отримує пароль користувача у параметр password, перевіряє його на збіг з паролем адміністратора у змінній ADMIN_PASSWORD і повертає повідомлення про результат порівняння.
+
+// Виконай рефакторинг коду функції checkPassword, використовуючи патерн «раннє повернення»:
+
+// видали змінну message
+// видали else
+// код повинен працювати так само, як і до оптимізації
+// Оголошена функція checkPassword(password)
+// Виклик checkPassword("mangohackzor") повертає "Access denied, wrong password!"
+// Виклик checkPassword("polyhax") повертає "Access denied, wrong password!"
+// Виклик checkPassword("jqueryismyjam") повертає "Welcome!"
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+
+
+  
+
+//   if (password === ADMIN_PASSWORD) {
+//    return "Welcome!";
+//   }
+//     return "Access denied, wrong password!";
+  
+
+// }
+
+// ********************************************************************
+// 3.
+// Функція checkStorage перевіряє можливість оформлення замовлення і повертає повідомлення про результат. Вона приймає два параметри, значення яких будуть задаватися під час її виклику.
+
+// available - доступна кількість товарів на складі
+// ordered - кількість одиниць товару в замовленні
+// Виконай рефакторинг коду функції checkStorage, використовуючи патерн «раннє повернення».
+
+// Оголошена функція checkStorage(available, ordered)
+// Виклик checkStorage(100, 50) повертає "The order is accepted, our manager will contact you"
+// Виклик checkStorage(100, 130) повертає "Your order is too large, not enough goods in stock!"
+// Виклик checkStorage(70, 0) повертає "Your order is empty!"
+// Виклик checkStorage(200, 20) повертає "The order is accepted, our manager will contact you"
+// Виклик checkStorage(200, 250) повертає "Your order is too large, not enough goods in stock!"
+// Виклик checkStorage(150, 0) повертає "Your order is empty!"
+
+// function checkStorage(available, ordered) {
+
+ 
+
+//   if (ordered === 0) {
+//      return "Your order is empty!";
+//   } if (ordered > available) {
+//     return "Your order is too large, not enough goods in stock!";
+//   }
+//     return "The order is accepted, our manager will contact you";
+
+// }
+
+// ********************************************************************
+// 4.
+// Оголоси змінну fruits і присвой їй масив фруктів - рядків "apple", "plum", "pear" і "orange".
+
+// Оголошена змінна fruits
+// Значення змінної fruits - це масив["apple", "plum", "pear", "orange"]
+
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// ********************************************************************
+// 5.
+// Оголоси три змінні і присвой кожній з них значення, використовуючи нотацію квадратних дужок.
+
+// Ім'я змінної	Значення змінної
+// firstElement	перший елемент масиву
+// secondElement	другий елемент масиву
+// lastElement	останній елемент масиву
+// Оголошена змінна firstElement
+// Значення змінної firstElement - це рядок "apple"
+// Оголошена змінна secondElement
+// Значення змінної secondElement - це рядок "plum"
+// Оголошена змінна lastElement
+// Значення змінної lastElement - це рядок "orange"
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// // Change code below this line
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits[3];
+// ********************************************************************
+// 6.
+// Виконай перевизначення значення елементів з індексами 1 і 3. Заміни "plum" на "peach", а "orange" на "banana".
+
+// Оголошена змінна fruits
+// Значення змінної fruits - це масив["apple", "peach", "pear", "banana"]
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// fruits[1] = "peach";
+// fruits[3] = "banana";
+
+// ********************************************************************
+// 7.
+// Оголоси змінну fruitsArrayLength і присвой їй довжину масиву fruits, використовуючи властивість length.
+
+// Оголошена змінна fruitsArrayLength
+// Значення змінної fruitsArrayLength - це число 4
+
+// const fruits = ["apple", "peach", "pear", "banana"];
+
+
+// const fruitsArrayLength = fruits.length;
+// ********************************************************************
+// 8.
+// Оголоси дві зміні:
+
+// Ім'я змінної	Очікуване значення
+// lastElementIndex	Індекс останнього елемента масиву fruits через довжина_масиву - 1
+// lastElement	Значення останнього елемента масиву
+// Оголошена змінна lastElementIndex
+// Значення змінної lastElementIndex - це число 3
+// Оголошена змінна lastElement
+// Значення змінної lastElement - це рядок "banana"
+
+// const fruits = ["apple", "peach", "pear", "banana"];
+
+// // Change code below this line
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex];
+
+// ********************************************************************
+// 9.
+// Напиши функцію getExtremeElements(array), яка приймає один параметр array - масив елементів довільної довжини. Функція повинна повертати масив з двох елементів - першого і останнього елемента параметра array.
+
+// Оголошена функція getExtremeElements(array)
+// Виклик getExtremeElements([1, 2, 3, 4, 5]) повертає [1, 5]
+// Виклик getExtremeElements(["Earth", "Mars", "Venus"]) повертає ["Earth", "Venus"]
+// Виклик getExtremeElements(["apple", "peach", "pear", "banana"]) повертає["apple", "banana"]
+
+// function getExtremeElements(array) {
+
+//   return [array[0], array[array.length - 1]];
+
+// }
+
+// ********************************************************************
+// 10.
+// Доповни код функції splitMessage(message, delimiter) таким чином, щоб вона повертала у змінній words результат поділу рядка message за роздільником delimiter - масив рядків.
+
+// Оголошена функція splitMessage(message, delimiter)
+// Виклик splitMessage("Mango hurries to the train", " ") повертає ["Mango", "hurries", "to", "the", "train"]
+// Виклик splitMessage("Mango", "") повертає ["M", "a", "n", "g", "o"]
+// Виклик splitMessage("best_for_week", "_") повертає["best", "for", "week"]
+
+// function splitMessage(message, delimiter) {
+//   let words;
+//   // Change code below this line
+  
+//    words = message.split(delimiter);
+
+//   // Change code above this line
+//   return words;
+// }
+// ********************************************************************
+// 11.
+// Сервісу гравірування прикрас потрібна функція, яка б автоматично рахувала ціну гравірування, залежно від кількості слів і ціни за слово.
+
+// Оголошена функція calculateEngravingPrice(message, pricePerWord). Ця функція приймає рядок, що складається зі слів, розділених лише пробілами (параметр message) та ціну гравірування одного слова (параметр pricePerWord).
+
+// Напиши тіло функції, щоб вона повертала загальну вартість гравірування усіх слів в рядку.
+
+// Оголошена функція calculateEngravingPrice(message, pricePerWord)
+// Виклик calculateEngravingPrice("JavaScript is in my blood", 10) повертає 50
+// Виклик calculateEngravingPrice("JavaScript is in my blood", 20) повертає 100
+// Виклик calculateEngravingPrice("Web-development is creative work", 40) повертає 160
+// Виклик calculateEngravingPrice("Web-development is creative work", 20) повертає 80
+
+// function calculateEngravingPrice(message, pricePerWord) {
+  
+//   const totalPrise = message.split(" ").length * pricePerWord;
+ 
+
+//    return totalPrise;
+// }
+
+// ********************************************************************
+// 12.
+// Доповни код функції makeStringFromArray(array, delimiter) таким чином, щоб вона повертала у змінній string результат з'єднання елементів масиву array з роздільником delimiter - рядок.
+
+// Оголошена функція makeStringFromArray(array, delimiter)
+// Виклик makeStringFromArray(["Mango", "hurries", "to", "the", "train"], " ") повертає "Mango hurries to the train"
+// Виклик makeStringFromArray(["M", "a", "n", "g", "o"], "")) повертає "Mango"
+// Виклик makeStringFromArray(["top", "picks", "for", "you"], "_") повертає "top_picks_for_you"
+
+// function makeStringFromArray(array, delimiter) {
+//   let string;
+//   // Change code below this line
+//        string = array.join(delimiter);
+
+
+//   // Change code above this line
+//   return string;
+// }
+
+
+// ********************************************************************
+// 13.
+// Термін slug - це людино-зрозумілий унікальний ідентифікатор, який використовується у веб-розробці для створення читабельних URL-адрес.
+
+// Наприклад, замість того, щоб користувач побачив в адресному рядку mysite.com/posts/1q8fh74tx, можна зробити slug з назви статті. В результаті адреса буде приємнішою для сприйняття: mysite.com/posts/arrays-for-begginers.
+
+// Увага
+// Slug - це завжди рядок у нижньому регістрі, слова якого розділені тире.
+
+// Напиши функцію slugify(title), яка приймає заголовок статті, параметр title, і повертає slug, створений з цього рядка.
+
+// Значенням параметра title будуть рядки, слова яких розділені лише пробілами
+// Усі символи slug повинні бути у нижньому регістрі
+// Всі слова slug повинні бути розділені тире
+// Оголошена функція slugify(title)
+// Виклик slugify("Arrays for begginers") повертає "arrays-for-begginers"
+// Виклик slugify("English for developer") повертає "english-for-developer"
+// Виклик slugify("Ten secrets of JavaScript") повертає "ten-secrets-of-javascript"
+// Виклик slugify("How to become a JUNIOR developer in TWO WEEKS") повертає "how-to-become-a-junior-developer-in-two-weeks"
+
+
+// function slugify(title) {
+//   // Change code below this line
+// let slug = title.toLowerCase().split(' ').join('-');
+
+
+//   // Change code above this line
+//   return slug;
+// }
+
+// ********************************************************************
+// 14.
+// Доповни код таким чином, щоб змінні містили часткові копії вихідного масиву fruits.
+
+// firstTwoEls - масив із перших двох елементів
+// nonExtremeEls - масив з усіх елементів, крім першого та останнього
+// lastThreeEls - масив із трьох останніх елементів
+// Оголошена змінна fruits
+// Значення змінної fruits - це масив ["apple", "plum", "pear", "orange", "banana"]
+// Оголошена змінна firstTwoEls
+// Значення змінної firstTwoEls - це масив ["apple", "plum"]
+// Оголошена змінна nonExtremeEls
+// Значення змінної nonExtremeEls - це масив ["plum", "pear", "orange"]
+// Оголошена змінна lastThreeEls
+// Значення змінної lastThreeEls - це масив ["pear", "orange", "banana"]
+// Змінній lastThreeEls присвоєна копія частини масиву fruits після застосування методу slice з правильними аргументами
+
+// const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+// // Change code below this line
+// const firstTwoEls = fruits.slice(0,2);
+// const nonExtremeEls = fruits.slice(1, fruits.length-1);
+// const lastThreeEls = fruits.slice(-3);
+
+// console.log(lastThreeEls);
+
+
+
+
